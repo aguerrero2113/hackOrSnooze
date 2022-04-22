c2e60f0f8c56f0363f019a3ed732123d6eec8ff6
Artemio Guerrero <aguerrero2113@gmail.com>
2022-04-21 20:26:11 -0700

    modified hackOrSnooze

--- js/main.js
+++ js/main.js
@@ -6,27 +6,40 @@ const $body = $("body");
 
 const $storiesLoadingMsg = $("#stories-loading-msg");
 const $allStoriesList = $("#all-stories-list");
+const $favoritedStories = $("#favorited-stories");
+const $ownStories = $("#my-stories");
+
+// selector that finds all three story lists
+const $storiesLists = $(".stories-list");
 
 const $loginForm = $("#login-form");
 const $signupForm = $("#signup-form");
 
+const $submitForm = $("#submit-form");
+
+const $navSubmitStory = $("#nav-submit-story");
 const $navLogin = $("#nav-login");
 const $navUserProfile = $("#nav-user-profile");
 const $navLogOut = $("#nav-logout");
 
+const $userProfile = $("#user-profile");
 /** To make it easier for individual components to show just themselves, this
  * is a useful function that hides pretty much everything on the page. After
  * calling this, individual components can re-show just what they want.
  */
 
-function hidePageComponents() {
+ function hidePageComponents() {
   const components = [
-    $allStoriesList,
+    $storiesLists,
+    $submitForm,
     $loginForm,
     $signupForm,
+    $userProfile
   ];
   components.forEach(c => c.hide());
 }
+
+/** Overall function to kick off the app. */
 
 /** Overall function to kick off the app. */
 
